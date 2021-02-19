0b93888d2334f99f7897aa8adadb8a7c98119c52
ManuelRojasR <m.rojas@lifeware.cl>
2021-02-19 18:13:49 -0300

    lifewaretutorial

--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,6 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
+    Lifeware: ['descargar-y-descomprimir-pictogramas','importar-pictogramas-a-integraboard']
   },
 };
