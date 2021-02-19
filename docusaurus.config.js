361c885ffa047b906d1abef31b4bbeaebadb99db
ManuelRojasR <m.rojas@lifeware.cl>
2021-02-19 18:52:31 -0300

    revert

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://ManuelRojasR.github.io',
-  baseUrl: '/',
+  baseUrl: '/docu/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
